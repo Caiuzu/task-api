42c6e346b5628a3637a06c7b787f096ec5895f58
Caio Souza <caio.7s@outlook.com>
2024-02-20 23:21:19 -0300

    feat<TASK-00>: implementando filtros genéricos adicionados com o comando 'nest g f /common/GenericExceptions'

---
+++ src/common/generic-exceptions/generic-exceptions.filter.spec.ts
@@ -0,0 +1,7 @@
+import { GenericExceptionsFilter } from './generic-exceptions.filter';
+
+describe('GenericExceptionsFilter', () => {
+  it('should be defined', () => {
+    expect(new GenericExceptionsFilter()).toBeDefined();
+  });
+});
